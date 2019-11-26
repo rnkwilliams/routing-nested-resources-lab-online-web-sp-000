a874362e4ebdcb1403f62b3e83da1e2310ae16e3
Ranika Williams <rnkprice07@gmail.com>
2019-11-26 21:19:26 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,15 @@
 class SongsController < ApplicationController
   def index
-    
+    if params[:artist_id]
+      if Artist.find_by(id: params[:artist_id])
+        @songs = Artist.find(params[:artist_id]).songs
+      else
+        flash[:alert] = "Artist not found"
+        redirect_to artists_path
+      end
+    else
     @songs = Song.all
+  end
   end
 
   def show
